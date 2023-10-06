98bf446fa0cd82812e44cd81a0556d5be53b9c1c
momoetdede <ro9178@hotmail.fr>
2023-10-06 11:27:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title("My parent's new healthy dinner")
+streamlit.title("My Parent's New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 1 Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Ranged Egg")
