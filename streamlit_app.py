5fb59156cff1527ff334f6e18c56ef79f4df5437
momoetdede <ro9178@hotmail.fr>
2023-10-06 11:31:27 +0200

    Update menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
-streamlit.title("My Parent's New Healthy Diner")
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 1 Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Ranged Egg")
+streamlit.title("My Parents' New Healthy Diner")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣Omega 3 1 Blueberry Oatmeal")
+streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔Hard-Boiled Free-Ranged Egg")
+streamlit.text("🥑🍞Avocado Toast")
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
